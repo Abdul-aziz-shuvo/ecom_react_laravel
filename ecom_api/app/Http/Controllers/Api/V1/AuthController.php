1f40c76f64a30b17c2f0a238a0a55bb9a4439b78
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2022-09-17 13:43:13 +0600

    first commit

---
+++ ecom_api/app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function registration(RegisterRequest $registerRequest) {
+            $user = User::create($registerRequest->validated());
+            return
+
+    }
+}
